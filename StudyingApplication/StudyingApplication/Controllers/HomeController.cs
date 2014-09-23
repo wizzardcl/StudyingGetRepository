84d5bdcfc4a13b01d0b3e16f71215029007995c2
unknown <Ivan_Dziameshka@EPBYMINW3328.minsk.epam.com>
2014-09-23 12:55:29 +0300

    Init rep

---
+++ StudyingApplication/StudyingApplication/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace StudyingApplication.Controllers
+{
+  public class HomeController : Controller
+  {
+    //
+    // GET: /Home/
+
+    public ActionResult Index()
+    {
+      var model = new TestTagBuilder("div");
+
+      return View(model);
+    }
+  }
+}
---
+++ StudyingApplication/StudyingApplication/Controllers/TagBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace StudyingApplication.Controllers
+{
+  public sealed class TestTagBuilder
+  {
+    private readonly string _tagName;
+    public void AddClass() { }
+    public void AddAttribute() { }
+    public void MergeAttribute() { }
+    public void RemoveClass() { }
+    public void SetInnerText() { }
+
+    public TestTagBuilder(string tagName)
+    {
+      _tagName = tagName;
+    }
+
+    public string TagName { get { return _tagName; } }
+
+    public MvcHtmlString ToHtmlString()
+    {
+      return new MvcHtmlString(string.Format("<{0}></{0}>", TagName));
+    }
+  }
+}
